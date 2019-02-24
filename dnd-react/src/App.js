9c11490eefc35dab62b91f22b3a76f347441e104
Matt <krebsm249@dupage.edu>
2019-02-23 23:17:25 -0600

    Resized the logo and added it to the navbar

--- dnd-react/src/App.js
+++ dnd-react/src/App.js
@@ -18,18 +18,21 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">DnD Character Creator</Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+          <Navbar.Brand href="#Home">
+            <img alt="" src={logo} className="App-logo"/>
+            {'DnD Character Creation'}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#Home">Home</Nav.Link>
               <Nav.Link href="#Characters">Characters</Nav.Link>
               <Nav.Link href="#Logout" onClick={this.logout}>Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-        <img src={logo} className="App-logo" alt="logo" />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <p>
            Welcome to the Dungeons and Dragons character generator!
         </p>
